c05e8b5f0b85a4ad85538e9c488368a3ceceee7a
fatymahmed <fatima.ahmed.muhsin@gmail.com>
2020-04-29 17:21:21 +0300

    Created routes folder for routes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+// import routes
+const userRoutes = require("./routes/user");
 
 // app
 const app = express();
@@ -20,9 +22,7 @@ mongoose
   .then(() => console.log("DB Connected"));
 
 // routes
-app.get("/", (req, res) => {
-  res.send("hello from node");
-});
+app.use("/api", userRoutes);
 
 const port = process.env.PORT || 8000;
 
