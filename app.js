3b9ad3ebb093d305c445b93e9c84cf152a60657d
fatymahmed <fatima.ahmed.muhsin@gmail.com>
2020-04-29 17:28:13 +0300

    Created controllers folder

--- app.js
+++ app.js
@@ -7,13 +7,6 @@ const userRoutes = require("./routes/user");
 // app
 const app = express();
 
-// db
-// mongoose
-//   .connect(process.env.DATABASE, {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//   })
-//   .then(() => console.log("DB connected"));
 mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
@@ -21,7 +14,7 @@ mongoose
   })
   .then(() => console.log("DB Connected"));
 
-// routes
+// routes middlewares
 app.use("/api", userRoutes);
 
 const port = process.env.PORT || 8000;
