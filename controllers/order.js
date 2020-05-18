323ed0516181d32e5459b895470a53996be4d81e
fatymahmed <fatima.ahmed.muhsin@gmail.com>
2020-05-18 21:01:20 +0300

    Create orders and save them to DB

---
+++ controllers/order.js
@@ -0,0 +1,12 @@
+const { errorHandler } = require("../helpers/dbHandlerError");
+const { Order, CartItem } = require("../models/order");
+exports.create = (req, res) => {
+  req.body.order.user = req.profile;
+  const order = new Order(req.body.order);
+  order.save((error, data) => {
+    if (error) {
+      return res.status(400).json({ error: errorHandler(error) });
+    }
+    res.json(data);
+  });
+};
